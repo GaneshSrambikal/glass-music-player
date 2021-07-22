6229d37a71386f2b2a0317052c05cf7f42edfba7
Ganesh Srambikal <ganesh.srambikal@gmail.com>
2021-07-22 18:20:20 +0530

    cleans code and format

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ var songCurTime =  document.getElementById("song-curtime");
 var songEndTime = document.getElementById("song-endtime");
 var nexttrack = document.getElementById("nexttrack");
 var trackname = document.getElementById("music-name");
-// var songDuration = audioplayer.duration;
-// var songCurTime = audioplayer.currentTime;
+
 // change theme
 function changeTheme(){
     theme.classList.toggle("light-theme");
@@ -41,13 +40,9 @@ function changeTheme(){
 // show playlists
 function showPlaylists(){
     playlist.classList.add("show-list")
-    // playlistContent.classList.add("visibles")
-    console.log("show list")
 }
 function closePlaylists(){
     playlist.classList.remove("show-list")
-    // playlistContent.classList.remove("visibles")
-    console.log("close list")
 }
 // show developer credits
 function showDevCreds(){
@@ -71,41 +66,29 @@ function playPause(){
     playbtn.classList.toggle("fa-play");
     if(playbtn.classList.contains("fa-play") == false){
         audioplayer.play();
-        // songEndTime.innerHTML = `0${endTimeStamp()}`;
-        // songCurTime.innerHTML = `0${(audioplayer.currentTime / 60).toFixed(2)}`;
         setSongsTimeStamps();
         curTimeStamp();
         setSeeker();
-        // document.getElementById(String(trackCount)).style.backgroundColor = '#636363';
-        // document.getElementById(String(trackCount)).style.color = '#faf9f9ee';
+        
     }else{
         audioplayer.pause();
     }
     
 }
-console.log(audioplayer.duration / 60)
-console.log(audioplayer.currentTime)
-// console.log(controlPoint.max = `0${((audioplayer.duration && (audioplayer.duration).toFixed(2) / 60).toFixed(2) )}`)
-console.log(playlist)
+
 function curTimeStamp(){
-    
        return setInterval(()=> {
             songCurTime.innerHTML = "0"+(audioplayer.currentTime / 60).toFixed(2);
         },999);
-    
 }
 function setSeeker(){
-    
     return setInterval(()=>{
         controlPoint.max = (audioplayer.duration && audioplayer.duration / 60 );
-        // controlPoint.min = (audioplayer.currentTime / 60).toFixed(1);
+        
         controlPoint.min = (audioplayer.currentTime / 60).toFixed(1);
     },999)
 }
-// function endTimeStamp(){
-//     var endtime = (audioplayer.duration / 60).toFixed(2) - 0.60;
-//     return endtime;
-// }
+
 // convert timestamp
 function convertTimeStamp(){
 
@@ -117,17 +100,15 @@ var totalTrack = 3;
 var songNames =[
     "The Weekend - I Feel it coming","Imagine Dragon - Thunder","Post Melone - Sunflower","Billie Eilish, Khalid - lovely","Queen- Bohemian Rhapsody","John Denver - Take me Home, Country Road","The Weekend - Save Your Tears"
 ]
-console.log(playlist.children[trackCount])
+
 // next track
 function nextTrack(){
-    console.log(trackCount)
-    // audioplayer.currentSrc = 'http://127.0.0.1:5500/Sleep%20Away.mp3';
+    
     if(trackCount == 6){
         trackCount = 0;
     }else{
         trackCount++;
     }
-    console.log("next track");
     audioplayer.innerHTML = `<source src="${trackCount}.mp3" type="audio/mpeg">`
     audioplayer.load();
     trackname.innerHTML = songNames[trackCount];
@@ -137,18 +118,15 @@ function nextTrack(){
         audioplayer.play();
         playbtn.classList.toggle("fa-play");
     },2000)
-    // document.getElementById(String(trackCount)).style.backgroundColor = '#636363';
-    // document.getElementById(String(trackCount)).style.color = '#faf9f9ee';
+    
 }
 // prev track
 function prevTrack(){
-    // audioplayer.currentSrc = 'http://127.0.0.1:5500/Sleep%20Away.mp3';
     if(trackCount == 0){
         trackCount = 6;
     }else{
         trackCount--;
     }
-    console.log("prev track");
     audioplayer.innerHTML = `<source src="${trackCount}.mp3" type="audio/mpeg">`
     audioplayer.load();
     trackname.innerHTML = songNames[trackCount];
@@ -161,20 +139,7 @@ function prevTrack(){
     
 }
 function playSelectedSong(id){
-    // for(let i=0;i <=6;i++){
-    //     if(i == id){
-    //         document.getElementById(String(id)).style.backgroundColor = '#636363';
-    //         document.getElementById(String(id)).style.color = '#faf9f9ee';
-    //     }
-    //     else{
-    //         document.getElementById(String(i)).style.backgroundColor = 'none';
-    //         document.getElementById(String(i)).style.color = '#636363';
-    //     }
-    // }
-    // document.getElementById(String(trackCount)).style.backgroundColor = '#636363';
-    // document.getElementById(String(trackCount)).style.color = '#faf9f9ee';
-    // document.getElementById(String(id)).style.backgroundColor = '#636363';
-    // document.getElementById(String(id)).style.color = '#faf9f9ee';
+    
     audioplayer.innerHTML = `<source src="${id}.mp3" type="audio/mpeg">`
     audioplayer.load();
     trackname.innerHTML = songNames[id];
@@ -187,5 +152,5 @@ function playSelectedSong(id){
     closePlaylists();
     
 }
-console.log(audioplayer.currentSrc)
+
 
